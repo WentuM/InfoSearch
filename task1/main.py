f5afd9c4492d5204b134644a59e256b12548e51f
danil <povarnitsindv@sovcombank.ru>
2023-02-19 18:28:07 +0300

    complete the first task

---
+++ task1/main.py
@@ -0,0 +1,47 @@
+from string import Formatter
+import requests
+index_file = open("index.txt", "w", encoding="utf-8")
+vikachka_file = open("выкачка.txt", "w", encoding="utf-8")
+
+BASE_URL = 'https://www.liveinternet.ru/rating/today.tsv?;page={page}'
+HEADERS = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
+}
+
+LOADS_PACKAGE_NAME = 'loads'
+
+def print_hi(name):
+    sites_count = 0
+    i = 0
+    while sites_count < 100:
+        try:
+            i += 1
+            print('page ' + str(i))
+            url = get_url_for_page(i)
+            pages_response = requests.get(url, headers=HEADERS)
+            urls = list(
+                map(lambda item: 'http://' + item.split("\t")[1].replace("/", ""),
+                    pages_response.text.split("\n")[1:30]))
+            print(urls)
+        except Exception:
+            continue
+        try:
+            for url in urls:
+                page_response = requests.get(url, headers=HEADERS)
+                vikachka_file.write(page_response.text)
+                index_file.write(str(sites_count) + '.' + url + "\n")
+                sites_count += 1
+
+        except Exception:
+            continue
+
+    index_file.close()
+    vikachka_file.close()
+
+def get_url_for_page(page: int) -> str:
+    return Formatter().format(BASE_URL, page=str(page))
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
